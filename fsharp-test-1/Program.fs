a3281712176cf54bd5185430b825948772558f7d
Mike Odnis <airwolf635@gmail.com>
2024-02-16 15:40:34 -0500

    Quick commit

---
+++ fsharp-test-1/Program.fs
@@ -0,0 +1,16 @@
+﻿module HelloSquare
+
+let x4 x =
+  let x = x * x
+  let x = x * x
+  x
+
+let myPassedVal = x4 2 |> x4
+
+let myComposedVal = (x4 >> x4) 2
+
+[<EntryPoint>]
+let main argv =
+    printf "myPassedVal: %A\n" myPassedVal
+    printf "myComposedVal: %A" myComposedVal
+    0
